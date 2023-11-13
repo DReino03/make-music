23a63c6fdb1fb90a9a2ae2956b7dd38ead8508b9
Daniel Reinosa Luque <114745664+DReino03@users.noreply.github.com>
2023-11-13 19:30:24 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,5 +2,5 @@ input.onButtonPressed(Button.A, function () {
     music.play(music.stringPlayable("C5 G B A F A C5 B ", 120), music.PlaybackMode.UntilDone)
 })
 input.onButtonPressed(Button.B, function () {
-    music.play(music.stringPlayable("E B C5 A B G A F ", 120), music.PlaybackMode.UntilDone)
+    music.play(music.stringPlayable("E D E D F D F F ", 120), music.PlaybackMode.UntilDone)
 })
