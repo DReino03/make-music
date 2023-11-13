08b3c3384109e50a2970223a2f0ed05e5854ab7a
Daniel Reinosa Luque <114745664+DReino03@users.noreply.github.com>
2023-11-13 19:30:57 +0100

    melodiapersonal

--- main.ts
+++ main.ts
@@ -2,5 +2,5 @@ input.onButtonPressed(Button.A, function () {
     music.play(music.stringPlayable("C5 G B A F A C5 B ", 120), music.PlaybackMode.UntilDone)
 })
 input.onButtonPressed(Button.B, function () {
-    music.play(music.stringPlayable("E D E D F D F F ", 120), music.PlaybackMode.UntilDone)
+    music.play(music.stringPlayable("E D E D F D G G ", 120), music.PlaybackMode.UntilDone)
 })
